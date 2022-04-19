cc1d8a1b07efd8ae889e9aae4e88d0a6592d3ca6
borachoidev <tsuilook@gmail.com>
2022-04-19 13:09:25 +0900

    🧹 chore: prettier 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import type { NextPage } from 'next'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const titleStyle = css({
   boxSizing: 'border-box',
   width: 300,
   height: 200,
   color: 'red',
-})
+});
 
 const Home: NextPage = () => {
   return (
     <Emotion>
       <h1 css={titleStyle}>red text</h1>
     </Emotion>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const Emotion = styled.div`
   background: #cceecc;
-`
+`;
