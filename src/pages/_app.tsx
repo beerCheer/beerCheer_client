da34609b1b3e59f916a7deb1d44d35504e91e4b4
borachoidev <tsuilook@gmail.com>
2022-04-20 15:39:54 +0900

    🧹 chore: storybook 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
-import reset from '../utils/reset';
+import reset from '../styles/reset';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
