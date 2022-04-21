24a27e57aeecbddafd1c880f43d603ccec185b8a
borachoidev <tsuilook@gmail.com>
2022-04-21 18:20:56 +0900

    ✨ feat: modal 컴포넌트 추가

---
+++ src/components/common/modal/modal.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Modal from '.';
+import { useArgs } from '@storybook/client-api';
+
+export default {
+  Component: Modal,
+  title: '모달',
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = ({ onClose, close, ...args }) => {
+  const [{ isOpen }, updateArgs] = useArgs();
+  const handleClose = () => updateArgs({ isOpen: !isOpen });
+  return (
+    <div>
+      <button onClick={() => updateArgs({ isOpen: !isOpen })}>Open Modal</button>
+      <Modal onClose={handleClose} close={close} {...args} />
+    </div>
+  );
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  isOpen: false,
+  onClose: () => {},
+  children: '모달 컨텐츠',
+  close: true,
+};
