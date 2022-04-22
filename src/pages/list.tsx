34fd224d839be553c1332f3803da4b70ce72457c
moonseonyeong <seon_moon@naver.com>
2022-04-23 03:12:10 +0900

    ✨ feat: list page 목업 및 반응형

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import HomeLayout from '../components/common/layout/layout';
+import Beer from '../components/common/beer/beer';
+
+import Data from '../components/main/dummy';
+
+import { ListContanier, ListContent, ListTitle } from '../styles/list';
+
+interface DummyProps {
+  id: number;
+  name: string;
+  score: number;
+  imageUrl: string;
+}
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <HomeLayout>
+      <ListContanier>
+        <ListTitle>전체 페이지</ListTitle>
+        <ListContent>
+          {Data.map((item: DummyProps) => {
+            return <Beer key={item.id} id={item.id} name={item.name} score={item.score} imageUrl={item.imageUrl} />;
+          })}
+        </ListContent>
+      </ListContanier>
+    </HomeLayout>
+  );
 };
 
 export default List;
--- src/styles/list/index.tsx
+++ src/styles/list/index.tsx
@@ -3,8 +3,29 @@ import styled from '@emotion/styled';
 export const ListContanier = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   padding: 30px 16px 100px 16px;
-  border: red solid 1px;
   width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  gap: 30px 0px;
+`;
+
+export const ListContent = styled.div`
+  display: grid;
+  z-index: -1;
+  font-size: ${props => props.theme['font-lg']};
+  grid-template-columns: 1fr;
+  grid-gap: 24px;
+
+  ${props => props.theme.mq.tablet} {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  ${props => props.theme.mq.laptop} {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+export const ListTitle = styled.div`
+  font-size: ${props => props.theme['font-xl']};
 `;
