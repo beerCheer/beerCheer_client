6da6cab5d4d66610768582519aeadc2f5a82dce7
moonseonyeong <seon_moon@naver.com>
2022-04-23 02:55:18 +0900

    💎 style: mainColor theme 내부로 위치 변경 및 type 지정

---
+++ src/styles/list/index.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const ListContanier = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 16px 100px 16px;
+  border: red solid 1px;
+  width: 100%;
+`;
