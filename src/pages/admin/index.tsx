5922aa032f5b470e08aa43e09138e1b2243e3ac9
moonseonyeong <seon_moon@naver.com>
2022-04-26 23:25:49 +0900

    ✨ feat: admin 메인페이지 목업

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,78 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import ArrowRightIcon from '../../components/common/@Icons/arrowRightIcon';
+import HomeLayout from '../../components/common/layout/layout';
+import {
+  AdminContainer,
+  Title,
+  UnderLine,
+  Section,
+  Article,
+  ArticleContent,
+  ArticleTitle,
+  FlexBox,
+  Text,
+} from '../../styles/admin';
+import AdminDuumy from './dummy';
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const router = useRouter();
+
+  return (
+    <AdminContainer>
+      <Title>어드민페이지</Title>
+      <UnderLine />
+      <Section>
+        <Article>
+          <ArticleTitle>
+            유저관리
+            <ArrowRightIcon onClick={() => router.push('/admin/user')} />
+          </ArticleTitle>
+          <ArticleContent>
+            <FlexBox>
+              <Text>닉네임</Text>
+              <Text>가입일자</Text>
+            </FlexBox>
+            <UnderLine />
+            {AdminDuumy.map(el => {
+              return (
+                <FlexBox key={el.id}>
+                  <Text>{el.name}</Text>
+                  <Text>{el.signUpdate}</Text>
+                </FlexBox>
+              );
+            })}
+          </ArticleContent>
+        </Article>
+        <Article>
+          <ArticleTitle>
+            댓글관리
+            <ArrowRightIcon onClick={() => router.push('/admin/comments')} />
+          </ArticleTitle>
+          <ArticleContent>
+            <FlexBox>
+              <Text>닉네임</Text>
+              <Text>등록일자</Text>
+            </FlexBox>
+            <UnderLine />
+
+            {AdminDuumy.map(el => {
+              return (
+                <FlexBox key={el.id}>
+                  <Text>{el.name}</Text>
+                  <Text>{el.signUpdate}</Text>
+                </FlexBox>
+              );
+            })}
+          </ArticleContent>
+        </Article>
+      </Section>
+    </AdminContainer>
+  );
 };
 
 export default Admin;
+
+Admin.getLayout = function getLayout(page: React.ReactElement) {
+  return <HomeLayout>{page}</HomeLayout>;
+};
