c7cfd43c4ae4b97eed540d7c4aa6d39babf7cc4c
borachoidev <tsuilook@gmail.com>
2022-04-28 14:47:54 +0900

    ✨ feat: 페이지네이션 타입  추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,10 @@ import { API_END_POINT } from './../constants/index';
 import axios from 'axios';
 
 export const API = axios.create({ baseURL: API_END_POINT });
+
+export interface IPagination<T> {
+  page: number;
+  totalResults: number;
+  totalPages: number;
+  rows: T;
+}
