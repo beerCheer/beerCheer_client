ac0533f5ddb9f88333e4c359b4bdb585934d54b6
해달 <85778994+moonseonyeong@users.noreply.github.com>
2022-05-06 00:14:33 +0900

    Update admin.ts

--- src/api/hook/admin.ts
+++ src/api/hook/admin.ts
@@ -3,19 +3,19 @@ import { getCommentList, getUserList } from '../fetcher/admin';
 import { IRequestCommentList, IRequestUserList } from '../types/admin';
 
 export const ADMIN_QUERY_KEY = {
-  USERLIST: 'USERLIST',
-  COMMENTSLIST: 'COMMENTSLIST',
+  USERS: 'USERS',
+	COMMENTS: 'COMMENTS',
 };
 
 export const useUserListQuery = ({ per_page = 10, page = 1, options }: IRequestUserList) => {
-  return useQuery([ADMIN_QUERY_KEY.USERLIST, page], () => getUserList({ page, per_page }), {
+  return useQuery([ADMIN_QUERY_KEY.USERS, page], () => getUserList({ page, per_page }), {
     keepPreviousData: true,
     ...options,
   });
 };
 
 export const useCommentListQuery = ({ per_page = 10, page = 1, options }: IRequestCommentList) => {
-  return useQuery([ADMIN_QUERY_KEY.COMMENTSLIST, page], () => getCommentList({ page, per_page }), {
+  return useQuery([ADMIN_QUERY_KEY.COMMENTS, page], () => getCommentList({ page, per_page }), {
     keepPreviousData: true,
     ...options,
   });
