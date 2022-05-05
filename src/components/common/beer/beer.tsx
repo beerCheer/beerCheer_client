f9eca30284c33481f17a7d8ccf0ba5d23f55807f
borachoidev <tsuilook@gmail.com>
2022-05-05 23:45:46 +0900

    🐛 fix: beer 컴포넌트 onClick 이벤트 타겟 수정

--- src/components/common/beer/beer.tsx
+++ src/components/common/beer/beer.tsx
@@ -1,26 +1,36 @@
 import HeartIcon from '../@Icons/heartIcon';
 import { BeerThumnail, ImageWrapper } from '../../../styles/detail';
 import { BeerContainer, Icon, BeerName, BeerScore, Description } from './styled';
+import { useMutation } from 'react-query';
+import { likeBeer } from '../../../api/fetcher/beers';
 
 const beer = '🍺';
 
 interface BeerProps {
+  id: number;
   name: string;
   rate: number;
   imageUrl: string;
   onClick?: () => void;
 }
 
-const Beer = ({ onClick, name, rate, imageUrl }: BeerProps) => {
+const Beer = ({ onClick, name, rate, imageUrl, id }: BeerProps) => {
+  const { mutateAsync } = useMutation(likeBeer);
+
   return (
-    <BeerContainer onClick={onClick}>
-      <ImageWrapper>
+    <BeerContainer>
+      <ImageWrapper onClick={onClick}>
         <BeerThumnail src={imageUrl} />
       </ImageWrapper>
-      <Icon>
+      <Icon
+        onClick={() => {
+          alert('좋아요');
+          mutateAsync(id);
+        }}
+      >
         <HeartIcon width={40} height={35} />
       </Icon>
-      <Description>
+      <Description onClick={onClick}>
         <BeerName>{name}</BeerName>
         <BeerScore>{beer.repeat(rate)}</BeerScore>
       </Description>
