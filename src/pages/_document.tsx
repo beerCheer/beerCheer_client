a1e1722195e389deed8958e4793c30afdf9eafa6
borachoidev <tsuilook@gmail.com>
2022-05-10 11:27:22 +0900

    📦 ci: cORS meta tag 추가

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
