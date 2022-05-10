18242c5fb18f137956e9cee1794cefa91daa29b2
borachoidev <tsuilook@gmail.com>
2022-05-10 22:29:16 +0900

    🐛 fix: mypage 관련 api 타입변경

--- src/api/hook/mypage.ts
+++ src/api/hook/mypage.ts
@@ -1,3 +1,4 @@
+import { BEER_QUERY_KEY } from './beers';
 import { useQuery } from 'react-query';
 import { getUserFavoritesBeers, getUserComments, getUserRates } from '../fetcher/mypage';
 import { USER_QUERY_KEY } from './users';
@@ -9,13 +10,15 @@ const MYPAGE_QUERY_KEY = {
 };
 
 export const useFavoritesBeers = () => {
-  return useQuery([MYPAGE_QUERY_KEY.FAVORITES, USER_QUERY_KEY.USERS], () => getUserFavoritesBeers());
+  return useQuery([BEER_QUERY_KEY.BEERS, MYPAGE_QUERY_KEY.FAVORITES, USER_QUERY_KEY.USERS], () =>
+    getUserFavoritesBeers()
+  );
 };
 
 export const useUserComments = () => {
-  return useQuery([MYPAGE_QUERY_KEY.COMMENT], () => getUserComments());
+  return useQuery([MYPAGE_QUERY_KEY.COMMENT, USER_QUERY_KEY.USERS], () => getUserComments());
 };
 
 export const useUserRates = () => {
-  return useQuery([MYPAGE_QUERY_KEY.RATES], () => getUserRates());
+  return useQuery([MYPAGE_QUERY_KEY.RATES, USER_QUERY_KEY.USERS], () => getUserRates());
 };
--- src/api/types/mypage/index.ts
+++ src/api/types/mypage/index.ts
@@ -1,48 +1,13 @@
 import { IBeer } from '../beers';
 
-export interface IFavoritesBeer {
-  id: number;
-  name: string;
-  tagline: string;
-  first_brewed: string;
-  description: string;
-  image_url: string;
-  abv: number;
-  ibu: number;
-  target_fg: number;
-  target_og: number;
-  ebc: number;
-  srm: number;
-  ph: number;
-  attenuation_level: number;
-  volume: {
-    value: number;
-    unit: string;
-  };
-  avg?: number;
-}
-
-export interface IFavoritesBeers {
-  totalResults: number;
-  result: IFavoritesBeer[];
-  message?: string;
-}
-
 export interface IComment {
   beerId: number;
   content: string;
   createdAt: string;
   beerName?: string;
 }
-
-export interface IComments {
+export interface IMyData<T> {
   totalResults: number;
-  result: IComment[];
-  message?: string;
-}
-
-export interface IRates {
-  totalResults: number;
-  result: IBeer[];
+  result: T;
   message?: string;
 }
